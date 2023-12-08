2044e58778421be2eb0582701fcecc0287bbe843
Allen Chou <f1470891079@gmail.com>
2023-12-08 19:37:10 +0800

    Read and install helper functions

--- conda_function/helper.py
+++ conda_function/helper.py
@@ -1,6 +1,8 @@
 import subprocess
 from typing import List, Text
 
+from packaging.requirements import Requirement
+
 
 def subprocess_run(args: List[Text]):
     try:
@@ -22,3 +24,21 @@ def subprocess_run(args: List[Text]):
         )
     except Exception as e:
         print(f"An error occurred: {e}")
+
+
+def read_requirements(file_path: Text = "requirements.txt") -> List["Requirement"]:
+    with open(file_path, "r") as file:
+        requirements = file.readlines()
+    return [Requirement(req) for req in requirements if req.strip()]
+
+
+def install_requirements(requirements: List["Requirement"]) -> None:
+    if not requirements:
+        return
+    subprocess_run(["pip", "install"] + [str(req) for req in requirements])
+
+
+def install_requirements_from_file(file_path: Text = "requirements.txt") -> None:
+    if not file_path:
+        return
+    subprocess_run(["pip", "install", "-r", file_path])
